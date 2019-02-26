502c4d8672856b75d8d70131298e3f1bba5bf451
Vxns <48022183+Vxns@users.noreply.github.com>
2019-02-26 16:34:28 +0100

    Add files via upload
    bot.user!

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,15 +2,15 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
     // Role Officer
-    if (!message.member.roles.find(r => r.name === 'Officer')) return message.channel.send('This requires the role: Officer')
+    if (!message.member.roles.find(r => (r.name === 'Officer' || r.name === 'GM'))) return message.channel.send('This requires the role: Officer or GM')
     // The Text output
     let bicon = bot.user.displayAvatarURL
-         let botEmbed = new Discord.RichEmbed()
-         .setDescription("Blutritter")
+         let botembed = new Discord.RichEmbed()
+         .setDescription("No Pants Bot")
          .setColor("#15f153")
          .setThumbnail(bicon)
-         .addField("Bot Name", bot.member.username)
-         .addField("Created On", bot.member.createdAt)
+         .addField("Bot Name", bot.user.username)
+         .addField("Created On", bot.user.createdAt)
          .addField("Dev", "<@311103692847710210>")
          .addField("Owner", "<@402780055861723138>")
 
@@ -18,7 +18,7 @@ module.exports.run = async (bot, message, args) => {
         if(!botchannel) return message.channel.send("Couldn't find bot-spamm channel.")
 
         message.delete().catch(O_o=>{})
-        botchannel.send(botEmbed)
+        botchannel.send(botembed)
      }
 
 module.exports.help = {
